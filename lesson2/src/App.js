3e18ad8bbcd3b37f9cf3b8a10285c1d8cc4949a3
OlehBabiak <oleg_bob@ukr.net>
2021-03-19 23:38:07 +0200

    create lesson2-2

--- lesson2/src/App.js
+++ lesson2/src/App.js
@@ -1,1 +1,37 @@
+import React, {Component} from 'react';
+import User from "./components/User/User";
 
+class App extends Component {
+	
+	state = {
+		users: []
+	}
+	
+	constructor(props, context) {
+		super(props, context);
+		
+	}
+	
+	componentDidMount() {
+		
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(value => value.json())
+			.then(value => {
+				this.setState({users: value})
+			})
+	}
+	
+	render() {
+		
+		let {users} = this.state
+		return (
+			<div>
+				{
+					users.map(value => <User item={value}/>)
+				}
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ lesson2/src/components/User/User.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+	render() {
+		let {item} = this.props
+		console.log(item)
+		return (
+			<div>
+				{item.id} - {item.name}
+			</div>
+		);
+	}
+}
+
+export default User;
